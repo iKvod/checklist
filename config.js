28f1b6e4309e70282c2a5bfe9920bd0865121ef4
Ravshan <rbazarbaev@gmail.com>
2017-02-17 21:33:27 +0600

    lkajflkj

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var config = {
-    //token: '324830524:AAF_QWatxxdsHbxrC2cB82jxKj8tZmd6wWE', // production token for telegram bot api
-     token: '308572160:AAG4WW0OA9qdLK1bakj7-edMxS-P1vriedY', //test token for telegram bot
+    token: '324830524:AAF_QWatxxdsHbxrC2cB82jxKj8tZmd6wWE', // production token for telegram bot api
+    //  token: '308572160:AAG4WW0OA9qdLK1bakj7-edMxS-P1vriedY', //test token for telegram bot
     botName: 'automatoChecklist_bot',
     mongoUrl: 'mongodb://127.0.0.1:27017/checklist',
     opt: {
@@ -10,10 +10,10 @@ var config = {
         pass: 'Automatodev',
         auth: { authdb: "checklist"}
     },
-    // ceoBotID : 78923920,
-    // managerBotID : 228106138,
-    ceoBotID:207925830,     //for testing
-    managerBotID:207925830 // for testing
+    ceoBotID : 78923920,
+    managerBotID : 228106138,
+    // ceoBotID:207925830,     //for testing
+    // managerBotID:207925830 // for testing
 };
 
 module.exports =  config;
