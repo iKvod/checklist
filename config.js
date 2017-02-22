21432929a3ea12151992d7e0058aeaabbeff9548
Ravshan <rbazarbaev@gmail.com>
2017-02-22 22:02:21 +0600

    sdasda

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ var config = {
         pass: 'Automatodev',
         auth: { authdb: "checklist"}
     },
-    ceoBotID : 266667910,
+    ceoBotID : 78923920,
     managerBotID : 228106138
     // ceoBotID:207925830,     //for testing
     // managerBotID:207925830 // for testing
