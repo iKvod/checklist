96bc68e84c5ac468163a151c598a67a4d3b6e17f
Ravshan <rbazarbaev@gmail.com>
2017-02-24 16:47:57 +0600

    token

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var config = {
-    // token: '324830524:AAF_QWatxxdsHbxrC2cB82jxKj8tZmd6wWE', // production token for telegram bot api
-    token: '308572160:AAG4WW0OA9qdLK1bakj7-edMxS-P1vriedY', //test token for telegram bot
+    token: '324830524:AAF_QWatxxdsHbxrC2cB82jxKj8tZmd6wWE', // production token for telegram bot api
+    // token: '308572160:AAG4WW0OA9qdLK1bakj7-edMxS-P1vriedY', //test token for telegram bot
     botName: 'automatoChecklist_bot',
     // mongoUrl: 'mongodb://127.0.0.1:27017/checklistTest',
     mongoUrl: 'mongodb://127.0.0.1:27017/checklist',
@@ -13,8 +13,8 @@ var config = {
     },
     // ceoBotID : 78923920,
     // managerBotID : 228106138
-    ceoBotID:207925830,     //for testing
-    managerBotID:207925830 // for testing
+    // ceoBotID:207925830,     //for testing
+    // managerBotID:207925830 // for testing
     //pip:"10.130.39.7"
    // pip:"127.0.0.1"
 };
